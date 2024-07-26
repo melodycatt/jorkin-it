76716993c6e95b30c84e4db08dcc14aa6833f21e
Melody <edward.lenzner@gmail.com>
2024-07-26 19:59:03 +1000

    builder for attack, none of the other existing status types need one

--- Assets/Scripts/Status/AttackStatus.cs
+++ Assets/Scripts/Status/AttackStatus.cs
@@ -9,12 +9,30 @@ public class AttackStatus : StatusEffect {
     public int op;
     public List<StatusEffect> stati;
 
-    public AttackStatus(HealthManager on): base(on) {
+    public AttackStatus(HealthManager on, List<StatusEffect> statuses, int op, float mult): base(on) {
+        this.mult = mult;
+        this.op = op;
+        stati = statuses;
         on.attackManager.Effects = on.attackManager.Effects.Concat(stati).ToList();
         on.attackManager.Modifiers.Add(this); 
     }
 
     public override StatusEffect Copy() {
-        return new AttackStatus(healthManager);
+        return new AttackStatus(healthManager, stati, op, mult);
+    }
+
+    public static class AttackStatusBuilder {
+        public static AttackStatus Adder(HealthManager on, float mult) {
+            return new AttackStatus(on, null, 0, mult);
+        }
+        public static AttackStatus Multer(HealthManager on, float mult) {
+            return new AttackStatus(on, null, 3, mult);
+        }
+        public static AttackStatus Divier(HealthManager on, float mult) {
+            return new AttackStatus(on, null, 4, mult);
+        }
+        public static AttackStatus Statifier(HealthManager on, List<StatusEffect> stati) {
+            return new AttackStatus(on, stati, 0, 0);
+        }
     }
 }
